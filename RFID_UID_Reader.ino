1deb547dd787e0f0b51eb4d5e08dfdaea62d82a6
Marcin Nowak <marcin94nowak@gmail.com>
2019-09-15 21:18:42 +0200

    Code cleaning
    Cleaned up code a bit.
    Now the project supports all UID lengths instead of most common 4-byte.

--- RFID_UID_Reader.ino
+++ RFID_UID_Reader.ino
@@ -1,14 +1,20 @@
 /* http://www.arduino.cc/en/Tutorial/LiquidCrystal */
 
-#include <EEPROM.h>                 //Library To read and write PICC's UIDs from/to EEPROM
-#include <LiquidCrystal.h>          //LCD 
-#include <MFRC522.h>                //RFID RC522 Module
-#include <SPI.h>                    //SPI
+#include <LiquidCrystal.h>            // LCD 
+#include <MFRC522.h>                  // RFID RC522 Module
+#include <SPI.h>                      // SPI
 
-#define RST_PIN         9           //Reset pin
-#define SS_PIN          10          //SlaveSelect pin
+#define RST_PIN         9             // MFRC522 Reset pin
+#define SS_PIN          10            // MFRC522 SlaveSelect pin
 
-LiquidCrystal lcd(8, 6, 5, 4, 3, 2);  //Initializing LCD PINS as (RS,EN,D4,D5,D6,D7)
+#define RS              8             // LCD     Register Select pin
+#define Enable          6             // LCD     Enable pin
+#define D4              5             // LCD     Digital 4 pin
+#define D5              4             // LCD     Digital 5 pin
+#define D6              3             // LCD     Digital 6 pin
+#define D7              2             // LCD     Digital 7 pin
+
+LiquidCrystal lcd(RS, Enable, D4, D5, D6, D7);  
 MFRC522 mfrc522(SS_PIN, RST_PIN);     // Create MFRC522 instance
 
 /* Typical pin layout used for RFID:
@@ -25,56 +31,46 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);     // Create MFRC522 instance
  */
 
 void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("RFID Reader");
+  
+  lcd.begin(16, 2);                                 // set up the LCD's number of columns and rows:
+  lcd.print(F("RFID Reader"));
   lcd.setCursor(0, 1);
-  lcd.print("Scan a card ...");
+  lcd.print(F("Scan a card ..."));
   Serial.begin(9600);                               // Initialize serial communications with the PC
   while (!Serial);                                  // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-  SPI.begin();                                      // Init SPI bus
-  mfrc522.PCD_Init();                               // Init MFRC522
+  SPI.begin();                                      
+  mfrc522.PCD_Init();                               
   mfrc522.PCD_DumpVersionToSerial();                // Show details of PCD - MFRC522 Card Reader details
   mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
 }
 
-int     screendelay=5000;                              // Variable screen delay
-int     successRead;                                  // Variable integer to keep if we have Successful Read from Reader
-byte    readCard[6];                                  // Stores scanned ID read from RFID Module
-String  CardID;
+int     screendelay=5000;
+int     timetoread=1000;                            // Time buffer ensuring successful retrieval of all information from card
+int     successRead;                                // Return code from the reader
+byte    readCard[10];                               // Stores scanned ID read from RFID Module
+String  CardID;                                     
 
 void loop() {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("RFID Reader");
+  lcd.print(F("RFID Reader"));
   lcd.setCursor(0, 1);
-  lcd.print("Scan a card ...");
-  do {successRead = getID();
-      Serial.println(F("No card so far ..."));}           // sets successRead to 1 when we get read from reader otherwise 0
-    while (!successRead);               //the program will not go further while you not get a successful read
-    for ( int j = 0; j < 4; j++ ) {     
-      EEPROM.write( j, readCard[j] );   // Write scanned PICC's UID to EEPROM, start from address 3
-    }
+  lcd.print(F("Scan a card ..."));
+  do {successRead = getID();}           // sets successRead to 1 when we get read from reader otherwise 0
+    while (!successRead);               // the program will not go further while you not get a successful read
+    
     // Dump debug info about the card; PICC_HaltA() is automatically called
     mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
-
     CardID="";
-    CardID+=String(readCard[0], HEX);
-    CardID+=String(readCard[1], HEX);
-    CardID+=String(readCard[2], HEX);
-    CardID+=String(readCard[3], HEX);
+    for (int i = 0; i < mfrc522.uid.size; i++) {  // for size of uid.size write uid.uidByte to readCard
+    readCard[i] = mfrc522.uid.uidByte[i];
+    CardID+=String(readCard[i], HEX);
+    };
     
-    Serial.print("Card: ");
-    Serial.print(readCard[0], HEX);
-    Serial.print(readCard[1], HEX);
-    Serial.print(readCard[2], HEX);
-    Serial.print(readCard[3], HEX);
-
     Serial.println(CardID);
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("Cards UID:");
+      lcd.print(F("Cards UID:"));
       lcd.setCursor(0, 1);
       lcd.print(CardID);    
       delay(screendelay);
@@ -83,29 +79,28 @@ void loop() {
 int getID() {
   // Getting ready for Reading PICCs
   if ( ! mfrc522.PICC_IsNewCardPresent()) { //If a new PICC placed to RFID reader continue
+    Serial.print(F("."));
     return 0;
-    Serial.println(F("No card yet..."));
   }
   if ( ! mfrc522.PICC_ReadCardSerial()) { //Since a PICC placed get Serial and continue
-    return 0;
     Serial.println(F("Reading serial ..."));
-  }
-  // There are Mifare PICCs which have 4 byte or 7 byte UID care if you use 7 byte PICC
-  // I think we should assume every PICC as they have 4 byte UID
-  // Until we support 7 byte PICCs
-
-  Serial.println("Scanning PICC's UID.........");
+    return 0;
+      }
+  
+  Serial.println(F("Scanning PICC's UID..."));
+  
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("SCANNING");
+  lcd.print(F("SCANNING"));
   lcd.setCursor(0, 1);
-  lcd.print("PICC's UID...");
-  delay(2000);
-  for (int i = 0; i < 4; i++) {  //
+  lcd.print(F("PICC's UID..."));
+  delay(timetoread);
+  
+  for (int i = 0; i < mfrc522.uid.size; i++) {  // for size of uid.size write uid.uidByte to readCard
     readCard[i] = mfrc522.uid.uidByte[i];
     Serial.print(readCard[i], HEX);
   }
-  Serial.println("");
+  Serial.println(F(""));
   mfrc522.PICC_HaltA(); // Stop reading
   return 1;
 }
